73daa32c2c6e448229dfea5b371ea1f6efd179e8
Logan738 <Logan.Snyder.514@Accesscreative.ac.uk>
2025-06-17 14:54:08 +0100

    AddedSpriteforball
    I added the bread sprite for the ball, and I went ahead and updated the map, as well as go ahead and started on some small sound design

--- Assets/LoganMiniGame/Balls/Ball.cs
+++ Assets/LoganMiniGame/Balls/Ball.cs
@@ -2,11 +2,16 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    
+    public AudioSource CollectBread;
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
+        
         if (other.CompareTag("Player"))
         {
             BallCounter.Instance.AddPoint();
+            CollectBread.Play();
             Destroy(gameObject);
         }
     }
